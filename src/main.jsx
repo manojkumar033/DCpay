c077b1c35639f9765abff07d08624bd05494ab79
manojkumar033 <60658898+manojkumar033@users.noreply.github.com>
2022-09-27 14:33:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
-import { TransactionsProvider } from "./context/TransactionContext";
+import { TransactionsProvider } from "./context/TransactionContext.jsx";
 import "./index.css";
 
 ReactDOM.render(
